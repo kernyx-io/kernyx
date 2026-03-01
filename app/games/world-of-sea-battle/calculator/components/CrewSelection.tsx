234540c41e745d4087ce16c1c0cedca7d83a5cf7
lkernyx <logankearney12@gmail.com>
2026-03-01 02:36:46 -0800

    Update CrewSelection.tsx

--- app/games/world-of-sea-battle/calculator/components/CrewSelection.tsx
+++ app/games/world-of-sea-battle/calculator/components/CrewSelection.tsx
@@ -1,4 +1,16 @@
+"use client"
+
+import type { CalculatorState } from "../lib/types"
+
 type Props = {
   state: CalculatorState
   dispatch: React.Dispatch<any>
+}
+
+export default function CrewSelection({ state, dispatch }: Props) {
+  return (
+    <div>
+      {/* UI here */}
+    </div>
+  )
 }
