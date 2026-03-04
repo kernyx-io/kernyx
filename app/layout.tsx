4811e190961297c35fa360336d87b0c34e44abd6
lkernyx <logankearney12@gmail.com>
2026-03-03 22:29:12 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,6 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
       </head>
-
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         style={{
